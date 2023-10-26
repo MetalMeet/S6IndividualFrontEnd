71ce1bd4a9ada6d0d4a7f60faeaaf9e1e4f5f3b5
I440389 <n.scheepens@student.fontys.nl>
2023-10-26 21:24:56 +0200

    changed test call to post

--- src/pages/home.js
+++ src/pages/home.js
@@ -24,7 +24,7 @@ const Home = () => {
     };
 
     axios
-      .get(`https://localhost:44337/api/event`, config)
+      .post(`https://localhost:44337/api/friendship`,{}, config)
       .then((res) => {
         const user = res.data;
         setUser(user);
